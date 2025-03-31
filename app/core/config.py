b0352907f77df5265b1fe48b6904ef4f479f0ffa
Дмитрий Лишухай <dlishuhai@yandex.ru>
2025-03-31 07:26:32 +0300

    Update to use base dir in config

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,20 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite+aiosqlite:///./db.sqlite3"
+    DATABASE_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
     TRON_API_KEY: str
 
     class Config:
-        env_file = (".env", ".env-example")
+        env_file = (
+            BASE_DIR / ".env-example",
+            BASE_DIR / ".env"
+        )
 
 
 settings = Settings()
